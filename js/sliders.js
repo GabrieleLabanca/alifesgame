5be863fd3d1e5f607953c758831831893cdffb52
inknos <nicola.sella@outlook.it>
2017-10-30 21:33:41 +0100

    second slider

--- js/sliders.js
+++ js/sliders.js
@@ -2,8 +2,9 @@ $( function() {
     var handle = $( "#time-slider" );
     $( "#time-div" ).slider({
         min: 8000,
-        max: 100000,
-	step:100,
+        max: 200000,
+	step: 1000,
+        value: 100000,
         create: function() {
             handle.text( $( this ).slider( "value" ) );
         },
@@ -11,6 +12,24 @@ $( function() {
             handle.text( ui.value ),
             clearInterval(timer),
             timer = setInterval(time_roll, ui.value/100)
+        }
+    });
+} );
+
+
+$( function() {
+    var handle = $( "#sheep-speed-slider" );
+    $( "#sheep-speed-div" ).slider({
+        min: 0,
+        max: 50,
+	step: 10,
+        value: 10,
+        create: function() {
+            handle.text( $( this ).slider( "value" ) );
+        },
+        slide: function( event, ui ) {
+            handle.text( ui.value ),
+            sheep_speed = ui.value;
         }
     });
 } );
