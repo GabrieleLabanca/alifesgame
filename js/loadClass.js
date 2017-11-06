fd2ae50b80fb19bbc29954d908761c7238d65e67
inknos <nicola.sella@outlook.it>
2017-11-06 20:14:29 +0100

    loadClass.js created

---
+++ js/loadClass.js
@@ -0,0 +1,27 @@
+var imported = document.createElement('script');
+imported.src = 'js/parameters.js';
+document.head.appendChild(imported);
+
+var imported2 = document.createElement('script');
+imported2.src = 'js/setupSvg.js';
+document.head.appendChild(imported2);
+
+var imported3 = document.createElement('script');
+imported3.src = 'js/Being.js';
+document.head.appendChild(imported3);
+
+var imported4 = document.createElement('script');
+imported4.src = 'js/graphic_update.js';
+document.head.appendChild(imported4);
+
+var imported5 = document.createElement('script');
+imported5.src = 'js/update.js';
+document.head.appendChild(imported5);
+
+var imported6 = document.createElement('script');
+imported6.src = 'js/main.js';
+document.head.appendChild(imported6);
+
+var imported7 = document.createElement('script');
+imported7.src = 'js/sliders.js';
+document.head.appendChild(imported7);
