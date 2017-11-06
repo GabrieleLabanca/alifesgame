a66f7e18d96c5c07f14bef32e3e3cd8ab3771c31
inknos <nicola.sella@outlook.it>
2017-11-06 20:20:16 +0100

    update loadClass.js

--- js/loadClass.js
+++ js/loadClass.js
@@ -2,13 +2,33 @@ var imported = document.createElement('script');
 imported.src = 'js/parameters.js';
 document.head.appendChild(imported);
 
+var imported1 = document.createElement('script');
+imported1.src = 'js/Being/functions.js';
+document.head.appendChild(imported1);
+
 var imported2 = document.createElement('script');
 imported2.src = 'js/setupSvg.js';
 document.head.appendChild(imported2);
 
-var imported3 = document.createElement('script');
-imported3.src = 'js/Being.js';
-document.head.appendChild(imported3);
+var imported3_1 = document.createElement('script');
+imported3_1.src = 'js/Being/Being.js';
+document.head.appendChild(imported3_1);
+
+var imported3_2 = document.createElement('script');
+imported3_2.src = 'js/Being/Animal.js';
+document.head.appendChild(imported3_2);
+
+var imported3_3 = document.createElement('script');
+imported3_3.src = 'js/Being/Plant.js';
+document.head.appendChild(imported3_3);
+
+var imported3_4 = document.createElement('script');
+imported3_4.src = 'js/Being/Sheep.js';
+document.head.appendChild(imported3_4);
+
+var imported3_5 = document.createElement('script');
+imported3_5.src = 'js/Being/Wolf.js';
+document.head.appendChild(imported3_5);
 
 var imported4 = document.createElement('script');
 imported4.src = 'js/graphic_update.js';
